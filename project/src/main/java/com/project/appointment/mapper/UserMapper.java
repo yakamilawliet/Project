45b33eff705190aa3d1c7c8e41bcd93ffd52bcc2
yakamilawliet <109347884+yakamilawliet@users.noreply.github.com>
2023-11-13 16:50:09 +0800

    Add files via upload

---
+++ project/src/main/java/com/project/appointment/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.project.appointment.mapper;
+
+import com.project.appointment.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 作者
+ * @since 2023-11-13
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
